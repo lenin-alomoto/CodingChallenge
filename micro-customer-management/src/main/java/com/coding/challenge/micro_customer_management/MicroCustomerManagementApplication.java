19ca43960d68e690d9530210ce0b3c7b5c820014
Lenin Alomoto <lenin.alomoto@hotmail.com>
2025-04-23 17:02:42 -0500

    Initial Project

---
+++ micro-customer-management/src/main/java/com/coding/challenge/micro_customer_management/MicroCustomerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.micro_customer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCustomerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCustomerManagementApplication.class, args);
+	}
+
+}
---
+++ micro-customer-management/src/test/java/com/coding/challenge/micro_customer_management/MicroCustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.micro_customer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
